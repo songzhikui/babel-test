7c000fd835945dfe3cf8c26f320187a97b5055d7
SongZhiKui <songzhikui@live.com>
2017-02-14 10:47:11 +0800

    a
    D

---
+++ src/script.js
@@ -0,0 +1,14 @@
+let foo = ['🍎','🍐'],
+    foods = [...foo,'🍰'];
+console.log(foods);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>nihao.com</h1>,
+    document.getElementById('app')
+);
+var a = 0;
+    b = 1;
+    c = 2;
